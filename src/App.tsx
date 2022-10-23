ef44360be4d03c8d530e2f577a8ead96cf0bd167
yourjcarter@gmail.com <yourjcarter@gmail.com>
2022-10-24 08:47:52 +1000

    Playing with typescript buttons and CSS added

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,15 @@
 import React, { FC } from 'react';
 import { Button } from 'antd';
+
 import './App.css';
 
 const App: FC = () => (
   <div className="App">
-    <Button type="primary">Button</Button>
+    <Button type="primary">Primary</Button>
+    <Button type="ghost">Ghost</Button>
+    <Button type="text">Text</Button>
+    <Button type="dashed">Dashed</Button>
+    <Button type="link">Link</Button>
   </div>
 );
 
